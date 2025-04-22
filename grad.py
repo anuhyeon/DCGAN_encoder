d26b36d99275fe1340590e27960433dc650e81ac
annuu <awh424@hanmail.net>
2025-04-22 10:11:56 +0000

    update gitignore

--- grad.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-x = torch.tensor(2.0, requires_grad=True)
-y = x * 3           # y = MulBackward0
-z = y + 5           # z = AddBackward0
-
-print(z.grad_fn)  # AddBackward0
-for i, fn in enumerate(z.grad_fn.next_functions):
-    print(f"{i}: {fn}")
